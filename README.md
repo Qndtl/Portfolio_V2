dd90cf634ac087d04d365a07a4eff741fa2e3eea
hyunduk-jo <johd321@kakao.com>
2021-04-15 22:05:07 +0900

    Finish

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Portfolio
 Portfolio with HTML, CSS, JS
+
+todo -> about: insert skills img, contact: github link & email
--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,28 @@ const mobileNav = document.querySelector('.mobile-nav');
 
 hamburger.addEventListener("click", () => {
   mobileNav.classList.remove('no-show');
+  window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
 })
 
 times.addEventListener("click", () => {
   mobileNav.classList.add('no-show');
+})
+
+const aboutMob = document.getElementById('about-mobile');
+const projectMob = document.getElementById('projects-mobile');
+const contactMob = document.getElementById('contact-mobile');
+
+aboutMob.addEventListener("click", () => {
+  mobileNav.classList.add('no-show');
+  window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
+})
+projectMob.addEventListener("click", () => {
+  mobileNav.classList.add('no-show');
+  window.scrollTo({ top: projects.offsetTop - 60, left: 0, behavior: 'smooth' })
+})
+contactMob.addEventListener("click", () => {
+  mobileNav.classList.add('no-show');
+  window.scrollTo({ top: contact.offsetTop - 60, left: 0, behavior: 'smooth' })
 })
 
 const aboutLi = document.querySelector('.about-li');
